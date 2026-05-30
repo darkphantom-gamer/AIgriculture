8eb2cb46f2f34b2f1b7b65559d239005406fccfd
The Great Himkamal <himkamal@gainway.in>
2026-05-30 14:12:28 +0530

    android: use official Square kotlinx-serialization converter for Retrofit 2.11

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -69,8 +69,8 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-kotlinx-serialization:2.11.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
 
     implementation("io.coil-kt:coil-compose:2.7.0")
 
